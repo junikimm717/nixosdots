5dd994c8d688f0c45e01fbba68789d50763f0155
Juni Kim <junikimm717@gmail.com>
2022-04-17 11:57:04 -0400

    ranger changes

--- configuration.nix
+++ configuration.nix
@@ -116,6 +116,7 @@ in
     dejavu_fonts 
     noto-fonts-emoji 
     iosevka
+    font-awesome
     nerdfonts
   ];
   environment.systemPackages = with pkgs; [
@@ -132,8 +133,26 @@ in
   services.mpd = {
     enable = true;
     musicDirectory = "/home/junikim/music";
+    dataDir = "/home/junikim/music/mpd";
     user = "junikim";
+    extraConfig = ''
+audio_output {
+        type            "pipewire"
+        name            "MPD Pipewire"
+}
+# adds fifo
+audio_output {
+    type                    "fifo"
+    name                    "my_fifo"
+    path                    "/tmp/mpd.fifo"
+    format                  "44100:16:2"
+}
+'';
   };
+systemd.services.mpd.environment = {
+    # https://gitlab.freedesktop.org/pipewire/pipewire/-/issues/609
+    XDG_RUNTIME_DIR = "/run/user/1000"; # User-id 1000 must match above user. MPD will look inside this directory for the PipeWire socket.
+};
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
