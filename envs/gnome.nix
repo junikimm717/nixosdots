3df74e72ac9e9988c81937a652bdcc87ba1fcb7f
Juni Kim <junikimm717@gmail.com>
2022-07-18 15:31:45 -0400

    gnome?

---
+++ envs/gnome.nix
@@ -0,0 +1,11 @@
+{ config, pkgs, ...}: {
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
+
+  environment.gnome.excludePackages = with pkgs; [
+    gnome.geary gnome.gnome-contacts gnome.gnome-weather gnome.cheese
+    gnome.gedit gnome.gnome-clocks gnome-connections gnome.yelp evince
+  ];
+
+  hardware.pulseaudio.enable = pkgs.lib.mkForce false;
+}
