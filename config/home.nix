455f9196a6d6b024916b170bdd86dd1ee0e8a423
Juni Kim <junikimm717@gmail.com>
2023-06-30 11:21:58 -0400

    update flake

--- config/home.nix
+++ config/home.nix
@@ -63,6 +63,7 @@ in {
     skypeforlinux
     zoom-us
     discord
+    whatsapp-for-linux
   ];
 
   home-manager.users.junikim = {
--- flake.nix
+++ flake.nix
@@ -2,17 +2,15 @@
   description = "Juni's Nix Flake dotfiles";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-22.11";
-    home-manager.url = "github:nix-community/home-manager/release-22.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
+    home-manager.url = "github:nix-community/home-manager/release-23.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
-    texbld.url = "github:texbld/texbld/master";
-    texbld.inputs.nixpkgs.follows = "nixpkgs";
     system76-keyboard-configurator.url = "github:pop-os/keyboard-configurator";
     system76-keyboard-configurator.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs = { self, nixpkgs, home-manager, system76-keyboard-configurator
-    , texbld, ... }:
+    , ... }:
     let
       commonModules = [
         home-manager.nixosModule
@@ -22,7 +20,7 @@
         ./config/home.nix
         ({ config, ... }: {
           environment.systemPackages = [
-            texbld.defaultPackage.x86_64-linux
+            #texbld.defaultPackage.x86_64-linux
             #system76-keyboard-configurator.defaultPackage.x86_64-linux
           ];
         })
